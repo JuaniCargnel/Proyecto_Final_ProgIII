6c43fcc865a5b267495315e4333b6c2ba356bd82
JuaniCargnel <JuaniCargnel@users.noreply.github.com>
2023-10-26 18:40:07 -0300

    Update 0.0.2

--- Scripts/personajePrincipal.gd
+++ Scripts/personajePrincipal.gd
@@ -1,35 +1,70 @@
 extends CharacterBody2D
 
-const velocidad = 200
+const velWalk = 75
+const velRun = 150
+const velRoll = 200
 
-func _physics_process(delta):
-	
-	var direcion = Vector2.ZERO
+var roll = false
+var can_roll = true
+var run = false
+var idle = true
+
+func _physics_process(_delta):
+	var direccion = Vector2()
 	
 	if Input.is_action_pressed("Right"):
-		direcion.x += 1
+		direccion.x += 1
 		$Sprite.set_flip_h(false)
-		$Sprite.play("run")
 	if Input.is_action_pressed("Left"):
-		direcion.x -= 1
+		direccion.x -= 1
 		$Sprite.set_flip_h(true)
-		$Sprite.play("run")
 	if Input.is_action_pressed("Up"):
-		direcion.y -= 1
-		$Sprite.play("run")
+		direccion.y -= 1
 	if Input.is_action_pressed("Down"):
-		direcion.y += 1
+		direccion.y += 1
+		
+	if Input.is_action_pressed("Correr") and direccion.x != 0 or Input.is_action_pressed("Correr") and direccion.y != 0:
+		run = true
+	else: 
+		run = false
+		
+	if Input.is_action_just_pressed("Roll") and can_roll:
+		roll = true
+		can_roll = false
+		$Roll.start()
+	else: 
+		roll = false
+		
+	if run:
+		velocity.x = direccion.normalized().x * velRun
+		velocity.y = direccion.normalized().y * velRun
+	else: 
+		velocity.x = direccion.normalized().x * velWalk
+		velocity.y = direccion.normalized().y * velWalk
+		
+	if run:
+		idle = false
 		$Sprite.play("run")
-	if direcion.x == 0 and direcion.y == 0:
-		$Sprite.play("idle")
-		
-	if direcion.length() > 0:
-		direcion = direcion.normalized()
-		
-	position += direcion * velocidad * delta
+	else:
+		if direccion != Vector2.ZERO and not roll:
+			$Sprite.play("walk")
+			idle = false
+		elif not idle:
+			idle = true
+			$Sprite.play("idle")
 	
-	position.x = clamp(position.x, 10, get_tree().root.content_scale_size.x)
-	position.y = clamp(position.y, 15, get_tree().root.content_scale_size.y)
+	if roll:
+		idle = false
+		run = false
+		$Sprite.play("roll")
+		velocity.x = direccion.normalized().x * velRoll
+		velocity.y = direccion.normalized().y * velRoll
 	
 	move_and_slide()
 
+func _on_roll_timeout():
+	roll = false
+	$RollTimer.start()
+
+func _on_roll_timer_timeout():
+	can_roll = true
