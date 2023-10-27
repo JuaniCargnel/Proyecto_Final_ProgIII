7dd821f89f0afcb40e2e79c994c203c13fbd6f54
JuaniCargnel <JuaniCargnel@users.noreply.github.com>
2023-10-27 17:14:55 -0300

    Update 0.0.3

--- Scripts/personajePrincipal.gd
+++ Scripts/personajePrincipal.gd
@@ -2,14 +2,27 @@ extends CharacterBody2D
 
 const velWalk = 75
 const velRun = 150
-const velRoll = 200
+const velRoll = 250
 
-var roll = false
 var can_roll = true
+var roll = false
 var run = false
 var idle = true
 
+func _ready():
+	$Sprite.play("idle")
+
 func _physics_process(_delta):
+	estados()
+	move_and_slide()
+	animations()
+
+func estados():
+	desplazamiento()
+	running()
+	rolling()
+
+func desplazamiento():
 	var direccion = Vector2()
 	
 	if Input.is_action_pressed("Right"):
@@ -23,44 +36,44 @@ func _physics_process(_delta):
 	if Input.is_action_pressed("Down"):
 		direccion.y += 1
 		
-	if Input.is_action_pressed("Correr") and direccion.x != 0 or Input.is_action_pressed("Correr") and direccion.y != 0:
-		run = true
-	else: 
-		run = false
-		
-	if Input.is_action_just_pressed("Roll") and can_roll:
-		roll = true
-		can_roll = false
-		$Roll.start()
-	else: 
-		roll = false
-		
 	if run:
 		velocity.x = direccion.normalized().x * velRun
 		velocity.y = direccion.normalized().y * velRun
+	elif roll:
+		velocity.x = direccion.normalized().x * velRoll
+		velocity.y = direccion.normalized().y * velRoll
 	else: 
 		velocity.x = direccion.normalized().x * velWalk
 		velocity.y = direccion.normalized().y * velWalk
-		
+
+func animations():
 	if run:
-		idle = false
 		$Sprite.play("run")
+	elif roll:
+		$Sprite.play("roll")
 	else:
-		if direccion != Vector2.ZERO and not roll:
+		if velocity != Vector2.ZERO and not roll:
 			$Sprite.play("walk")
 			idle = false
-		elif not idle:
+		elif not idle and not roll:
 			idle = true
 			$Sprite.play("idle")
-	
-	if roll:
-		idle = false
+
+func running():
+	if Input.is_action_pressed("Correr") and not roll and not idle and velocity != Vector2.ZERO:
+		run = true
+	else: 
 		run = false
-		$Sprite.play("roll")
-		velocity.x = direccion.normalized().x * velRoll
-		velocity.y = direccion.normalized().y * velRoll
-	
-	move_and_slide()
+
+func rolling():
+	if Input.is_action_pressed("Roll") and not idle and can_roll:
+		roll = true
+		can_roll = false
+		$Roll.start()
+	elif roll:
+		pass
+	else: 
+		roll = false
 
 func _on_roll_timeout():
 	roll = false
